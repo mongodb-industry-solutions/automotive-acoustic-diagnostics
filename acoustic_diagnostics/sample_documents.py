e605cd209e8d1fdd22031ccb1d69fb10b6071785
RamiPinto <rami.pinto@mongodb.com>
2024-08-12 13:42:12 +0200

    Add engine simulator

---
+++ acoustic_diagnostics/sample_documents.py
@@ -0,0 +1,59 @@
+running_normally_doc =  {
+            "sensor": "Engine Demo",
+            "results": [
+                {
+                "audio": "Running Normally",
+                "audio_file": "0",
+                "image": "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExaHVyNGh6bTFtcXZwMHZ0NzdqY2c5czV3NDNycTVxcTRxOWE3NzIzYiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/WSqsdbIH6mLrHe78tJ/giphy.gif",
+                "score": {
+                    "$numberDouble": "0.9386165738105774"
+                }
+                },
+                {
+                "audio": "Engine Off",
+                "audio_file": "0",
+                "image": "https://media.tenor.com/dOAWtNUHo8sAAAAC/running-is-impossible-work-out.gif",
+                "score": {
+                    "$numberDouble": "0.9130526781082153"
+                }
+                },
+                {
+                "audio": "Soft Material Hit",
+                "audio_file": "0",
+                "image": "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExc2tnNGR5ZGg1YmxtOTB0cnd3NWY4OXY3bzRpb3Vhbm01ODRvMDFtYyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/MdkW7kosS2c0QJzlni/giphy.gif",
+                "score": {
+                    "$numberDouble": "0.8546860218048096"
+                }
+                }
+            ]
+        }
+
+soft_hit_doc = {
+  "sensor": "Engine Demo",
+  "results": [
+    {
+      "audio": "Soft Material Hit",
+      "audio_file": "0",
+      "image": "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExc2tnNGR5ZGg1YmxtOTB0cnd3NWY4OXY3bzRpb3Vhbm01ODRvMDFtYyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/MdkW7kosS2c0QJzlni/giphy.gif",
+      "score": {
+        "$numberDouble": "0.9034188985824585"
+      }
+    },
+    {
+      "audio": "Metallic Hit",
+      "audio_file": "0",
+      "image": "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExc2tnNGR5ZGg1YmxtOTB0cnd3NWY4OXY3bzRpb3Vhbm01ODRvMDFtYyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/MdkW7kosS2c0QJzlni/giphy.gif",
+      "score": {
+        "$numberDouble": "0.8615078926086426"
+      }
+    },
+    {
+      "audio": "Running Normally",
+      "audio_file": "0",
+      "image": "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExaHVyNGh6bTFtcXZwMHZ0NzdqY2c5czV3NDNycTVxcTRxOWE3NzIzYiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/WSqsdbIH6mLrHe78tJ/giphy.gif",
+      "score": {
+        "$numberDouble": "0.820048987865448"
+      }
+    }
+  ]
+}
