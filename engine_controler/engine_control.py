7342310fdcc9c15da3128bc1d82037f22b882535
RamiPinto <rami.pinto@mongodb.com>
2024-11-05 16:29:40 +0100

    controller update

--- engine_controler/engine_control.py
+++ engine_controler/engine_control.py
@@ -1,6 +1,6 @@
 from gpiozero import InputDevice, LED
 from time import sleep
-from database import initial_sync, vehicle
+import vehicle
 
 # Initialize the sensor as a digital input device on GPIO 4
 sensor = InputDevice(4)
@@ -11,6 +11,7 @@ red = LED(12)    # Red LED connected to GPIO pin 12
 yellow = LED(6) # Yellow LED connected to GPIO pin 6
 green = LED(13)  # Green LED connected to GPIO pin 13
 relay = LED(16) # Relay connected to GPIO pin 16
+relay.value = 1
 
 def set_engine_status(status):
     """Control relay based on the engine status."""
@@ -20,29 +21,29 @@ def set_engine_status(status):
         relay.on()   # Turn off the engine
 
 def check_sensor():
-    global vehicle
-
-    if not vehicle:
+    if not vehicle.data:
         print("Vehicle data not found.")
         return
     elif sensor.is_active:
         print("No obstacle detected")
     else:
-        if vehicle.LightsOn:
+        if vehicle.data.LightsOn:
             print("Turning off")
             set_engine_status("stop")
-            vehicle.LightsOn = False
+            vehicle.data.LightsOn = False
         else:
             print("Turning on")
             set_engine_status("start")
-            vehicle.LightsOn = True
+            vehicle.data.LightsOn = True
         print("Obstacle detected")
 
 
 def main():
-    initial_sync()  # Load vehicle data from MongoDB
+    vehicle.initial_sync()  # Load vehicle data from MongoDB
+    print(vehicle.data.__dict__)
     while True:
         check_sensor()
+        print(vehicle.data.__dict__)
         sleep(1)
 
 if __name__ == "__main__":
--- engine_controler/vehicle.py
+++ engine_controler/vehicle.py
@@ -1,6 +1,6 @@
-# database.py
 import os
 from pymongo import MongoClient
+from bson import ObjectId
 from vehicle_model import Vehicle
 from dotenv import load_dotenv
 
@@ -10,17 +10,17 @@ MONGODB_URI = os.getenv("MONGODB_URI")
 VEHICLE_ID = os.getenv("VEHICLE_ID")
 
 client = MongoClient(MONGODB_URI)
-db = client.get_database("connected_vehicle")
-vehicles_collection = db.get_collection("vehicle_data")
+db = client["connected_vehicle"]
+coll = db["vehicle_data"]
 
 # Global vehicle instance
-vehicle = None
+data = None
 
 def initial_sync():
-    global vehicle
+    global data
     # Fetch the vehicle data using VEHICLE_ID
-    vehicle_data = vehicles_collection.find_one({"_id": VEHICLE_ID})
+    vehicle_data = coll.find_one({"_id": ObjectId(VEHICLE_ID)})
     if vehicle_data:
-        vehicle = Vehicle(vehicle_data)
+        data = Vehicle(vehicle_data)
     else:
         print("Vehicle data not found.")
