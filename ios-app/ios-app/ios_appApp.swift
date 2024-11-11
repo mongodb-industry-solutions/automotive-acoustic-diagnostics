c3d63c7712bfcdedc7f7e55ab822ab857eaeb93e
RamiPinto <rami.pinto@mongodb.com>
2024-11-11 12:51:43 +0100

    Initial commit new iOS app

---
+++ ios-app/ios-app/ios_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_appApp.swift
+//  ios-app
+//
+//  Created by Rami Pinto on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ios_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
